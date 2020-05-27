79c03cb470c8a8a7615c1fa8d65f9c18a6f0bcf9
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-05-27 12:22:17 +0200

    Add white lines after some of the files

--- blueprints/grafana/update.sh
+++ blueprints/grafana/update.sh
@@ -5,4 +5,3 @@ iocage exec "${1}" service grafana stop
 iocage exec "${1}" sysrc grafana_conf="/config/grafana.conf"
 iocage exec "${1}" sysrc grafana_enable="YES" 
 iocage exec "${1}" service grafana start
-
