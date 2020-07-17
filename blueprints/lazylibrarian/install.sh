e7f4be40f14d2f5cb612fb4e800d09032db4ad0f
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-07-17 17:04:06 +0200

    Fixup the PR

--- blueprints/lazylibrarian/install.sh
+++ blueprints/lazylibrarian/install.sh
@@ -14,14 +14,10 @@ createmount "$1" "${global_dataset_media}"/audiobooks /media/audiobooks
 createmount "$1" "${global_dataset_media}"/books /media/books
 createmount "$1" "${global_dataset_media}"/comics /media/comics
 createmount "$1" "${global_dataset_media}"/magazines /media/magazines
-
 iocage exec "$1" "git clone https://gitlab.com/LazyLibrarian/LazyLibrarian.git /usr/local/share/lazylibrarian"
-iocage exec "$1" "git clone git://github.com/urllib3/urllib3.git /urllib3"
-iocage exec "$1" "python /urllib3/setup.py install"
-iocage exec "$1" "rm -r /urllib3"
 iocage exec "$1" "pw user add lazylibrarian -c lazylibrarian -u 111 -d /nonexistent -s /usr/bin/nologin"
 iocage exec "$1" chown -R lazylibrarian:lazylibrarian /usr/local/share/lazylibrarian /config
-iocage exec "$1" mkdir /usr/local/etc/rc.d
+iocage exec "$1" mkdir /usr/local/etc/rc.d || true
 cp "${includes_dir}/lazylibrarian.rc" "${jail_root}/usr/local/etc/rc.d/lazylibrarian"
 iocage exec "$1" chmod u+x /usr/local/etc/rc.d/lazylibrarian
 iocage exec "$1" sysrc "lazylibrarian_enable=YES"
